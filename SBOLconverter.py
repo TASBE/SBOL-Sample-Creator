d050a778cfc068fa395f4eefd5bf739509e5004d
dashaveraksa <dveraksa@bu.edu>
2019-08-08 11:31:12 -0400

    updating jupyter notebook

--- SBOLconverter.py
+++ SBOLconverter.py
@@ -462,40 +462,7 @@ def NewProjUpload(username, password, doc):
     return(0)
 
 
-# new function to call Google API and check if the LCP dictionary has the component
-
-# def LCPDictionaryCaller():
-#     import gspread
-#     from oauth2client.service_account import ServiceAccountCredentials
-#     # use creds to create a client to interact with the Google Drive API
-#     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-#     creds = ServiceAccountCredentials.from_json_keyfile_name('client_info.json', scope)
-#     client = gspread.authorize(creds)
-
-#     #opening the spreadsheet
-#     spreadsheet = client.open("LCP Project Dictionary")
-#     worksheetNames = ['Reagent', 'Strain', 'Genetic Construct']
-#     existingNamesDict = {}
-#     for sheetName in worksheetNames:
-#         currentSheet = spreadsheet.worksheet(sheetName)
-#         currentInfo = currentSheet.get_all_records(False,2,'',False)
-#         if len(currentInfo) != 0:
-#             for index in range(0,len(currentInfo)):
-#                 currentDictionary = currentInfo[index]
-#                 existingNamesDict[currentDictionary['Common Name']] = currentDictionary['SynBioHub URI']
-#                 keysList = currentDictionary.keys()
-#                 for key in keysList:
-#                     if 'UID' in key and currentDictionary[key] is not '':
-#                         if ',' in currentDictionary[key]: # parses a list of UID's that are separated by a comma into separate entries
-#                             tempkeylist = currentDictionary[key].split(',')
-#                             for tempkey in tempkeylist:
-#                                 if tempkey[0] == ' ': # getting rid of any spaces that might have remained after the parsing
-#                                     tempkey = tempkey[1:] 
-#                                 existingNamesDict[tempkey] = currentDictionary['SynBioHub URI']
-#                         else:
-#                             existingNamesDict[(currentDictionary[key])] = currentDictionary['SynBioHub URI']
-#     return existingNamesDict
-      
+# function to call Google API and check if the LCP dictionary has the component
 # taken from Google Sheets example API call:
 def LCPDictionaryCaller():
     existingNamesDict = {}
